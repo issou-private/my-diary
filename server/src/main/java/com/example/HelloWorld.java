974a43144312c76e4da85a1ba41a3659a8b1d242
kazumaisoda <ikazuma1107@gmail.com>
2025-04-05 13:19:30 +0900

    add package.json for react and java HelloWorld class, file for test

---
+++ server/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
