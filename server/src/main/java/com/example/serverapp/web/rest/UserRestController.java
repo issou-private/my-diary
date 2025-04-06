218e93dbe65fb3800c348bcb16ad691a6cf1588c
kazumaisoda <ikazuma1107@gmail.com>
2025-04-06 10:06:14 +0900

    fixed to erase errors

--- server/src/main/java/com/example/serverapp/web/rest/UserRestController.java
+++ server/src/main/java/com/example/serverapp/web/rest/UserRestController.java
@@ -3,9 +3,6 @@ package com.example.serverapp.web.rest;
 import com.example.serverapp.persistence.entity.User;
 import com.example.serverapp.service.UserService;
 
-
-import com.example.serverapp.web.request.UserRequest;
-
 import com.example.serverapp.web.response.UserResponse;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -48,24 +45,4 @@ public class UserRestController {
         );
         return userResponse;
     }
-
-    @PostMapping
-    public LoginUserResponse insert(@RequestBody @Validated UserRequest request){
-        try{
-            User user = new User();
-            user.setUserName(request.getUserName());
-            user.setPassword(request.getPassword());
-            user.setJoinDate(request.getJoinDate());
-            userService.insert(user);
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
-        Integer id = userService.login(request.getEmail(), request.getPassword());
-        LoginUserResponse loginUserResponse = new LoginUserResponse(id);
-        return loginUserResponse;
-
-    }
-
 }
