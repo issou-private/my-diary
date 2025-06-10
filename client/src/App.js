ebdc65cfa1fa30c6208fb34d479c2f4efee11080
kazumaisoda <ikazuma1107@gmail.com>
2025-06-10 23:34:47 +0900

    changed initial message

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function App() {
+const App = () => {
+  //const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <div>
       <h1>My Diary</h1>
