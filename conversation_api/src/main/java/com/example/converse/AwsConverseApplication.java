49eea5802be3a5912637e5fdcabd83db7066fc0c
kazumaisoda <ikazuma1107@gmail.com>
2025-08-06 00:27:38 +0900

    fix main class

--- conversation_api/src/main/java/com/example/converse/AwsConverseApplication.java
+++ conversation_api/src/main/java/com/example/converse/AwsConverseApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.example.converse;
+package com.example.converse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
